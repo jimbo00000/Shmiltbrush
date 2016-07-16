269ae0faf99f2b905699f5ef80a2931716700269
Jim Susinno <james.susinno@gmail.com>
2016-07-16 11:56:30 -0400

    Log Vive button presses.

--- js/ViveController.js
+++ js/ViveController.js
@@ -22,6 +22,8 @@ THREE.ViveController = function ( id ) {
 			scope.matrix.compose( scope.position, scope.quaternion, scope.scale );
 			scope.matrix.multiplyMatrices( scope.standingMatrix, scope.matrix );
 			scope.matrixWorldNeedsUpdate = true;
+			scope.buttons = gamepad.buttons;
+			scope.axes = gamepad.axes;
 
 			scope.visible = true;
 
